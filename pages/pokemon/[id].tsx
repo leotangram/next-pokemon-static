031b8832062082dfba51a9a95a8cc886e9efc1b2
Leo Omaña <leotangram@gmail.com>
2022-04-04 15:48:29 -0500

    Add re-rendering static information

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -108,17 +108,29 @@ export const getStaticPaths: GetStaticPaths = async ctx => {
     paths: pokemons151.map(id => ({
       params: { id }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string }
 
+  const pokemon = await getPokemonInfo(id)
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
-    }
+      pokemon
+    },
+    revalidate: 86400
   }
 }
 
